1feea9f6b9c8bdd1da5ab29e5fe7be4e2f7beec6
Max Howell <mxcl@me.com>
2025-02-08 16:19:00 -0500

    +ca-certs

--- lib/fixup.ts
+++ lib/fixup.ts
@@ -47,3 +47,49 @@ export default async function fixup(path: Path, PKGX_DIR: Path) {
     console.log("::endgroup::");
   }
 }
+
+import { walk as std_walk } from "jsr:@std/fs@1/walk";
+
+type FixupType = 'shebang' | 'binary'
+
+async function* walk(prefix: Path): AsyncGenerator<string> {
+  for (const basename of ["bin", "lib", "libexec"]) {
+    const d = prefix.join(basename).isDirectory();
+    if (!d) continue;
+    for await (
+      const entry of std_walk(prefix.join(basename).string, {
+        includeFiles: true,
+        includeDirs: false,
+      })
+    ) {
+      try {
+        using file = await Deno.open(entry.path, { read: true });
+        const buffer = new Uint8Array(4);
+        await file.read(buffer);
+
+        if (buffer[0] === 35 && buffer[1] === 33) {
+          yield entry.path;
+        }
+
+        // Check for ELF (Linux/macOS) or Mach-O (macOS) headers
+        if (
+          buffer[0] === 0x7f && buffer[1] === 0x45 && buffer[2] === 0x4c &&
+          buffer[3] === 0x46
+        ) {
+          // ELF file (Linux/macOS executables and shared libraries)
+          yield entry.path;
+        } else if (
+          (buffer[0] === 0xcf && buffer[1] === 0xfa && buffer[2] === 0xed &&
+            buffer[3] === 0xfe) ||
+          (buffer[0] === 0xca && buffer[1] === 0xfe && buffer[2] === 0xba &&
+            buffer[3] === 0xbe)
+        ) {
+          // Mach-O file (macOS executables and shared libraries)
+          yield entry.path;
+        }
+      } catch (error) {
+        console.error(`::error::reading file ${entry.path}:`, error);
+      }
+    }
+  }
+}

0fd8fbc2834295e21aa311b1c5752114e5fc86a3
Max Howell <mxcl@me.com>
2025-02-12 14:41:50 -0500

    +zlib.net

---
+++ projects/zlib.net/test.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "jsr:@std/assert@^1";
+import { run } from "brewkit";
+
+export default async function () {
+
+  run`cc test.c -lz`;
+
+  const sample = 'Hello, World!'; //TODO random
+
+  const compress = new Deno.Command('./a.out', { stdin: 'piped', stdout: 'piped' }).spawn();
+  const decompress = new Deno.Command('./a.out', { args: ['-d'], stdin: 'piped', stdout: 'piped' }).spawn();
+
+  compress.stdout.pipeTo(decompress.stdin);
+
+  const writer = await compress.stdin.getWriter()
+  writer.write(new TextEncoder().encode(sample));
+  writer.close();
+
+  const output = new TextDecoder().decode((await decompress.output()).stdout);
+
+  assertEquals(output, sample);
+}

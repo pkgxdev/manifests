6d21fbc592facd0c80a1cbb59fcac16c26ad7977
Max Howell <mxcl@me.com>
2025-02-13 17:19:49 -0500

    +xz

---
+++ projects/tukaani.org/xz/build.ts
@@ -0,0 +1,10 @@
+import { BuildOptions, run, unarchive } from "brewkit";
+
+export default async function ({ prefix, tag, version }: BuildOptions) {
+  await unarchive(
+    `https://github.com/tukaani-project/xz/releases/download/${tag}/xz-${version}.tar.xz`,
+  );
+
+  run`./configure --prefix=${prefix} --disable-debug --disable-doc`;
+  run`make --jobs ${navigator.hardwareConcurrency} install`;
+}

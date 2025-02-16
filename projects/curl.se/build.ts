369f615b12d1208b279991dd6af4c584ba071e92
Max Howell <mxcl@me.com>
2025-02-15 19:04:18 -0500

    +curl

---
+++ projects/curl.se/build.ts
@@ -0,0 +1,32 @@
+import { BuildOptions, inreplace, run, unarchive } from "brewkit";
+
+export default async function ({ prefix, version }: BuildOptions) {
+  await unarchive(`https://curl.se/download/curl-${version}.tar.bz2`);
+
+  const args = [
+    `--prefix=${prefix}`,
+    "--without-libpsl", //causes weird downstream bugs in php
+  ];
+
+  switch (Deno.build.os) {
+    case "linux":
+      args.push(
+        "--with-openssl",
+        "--with-ca-fallback", //uses openssl’s certs (which we control)
+      );
+      break;
+    case "darwin":
+      args.push(
+        "--with-secure-transport", // use macOS’s native TLS
+      );
+  }
+
+  run`./configure ${args.join(" ")}`;
+  run`make --jobs ${navigator.hardwareConcurrency} install`;
+
+  inreplace(
+    prefix.join("bin/curl-config"),
+    /^prefix='.+'$/gm,
+    'prefix="$(cd "$(dirname "$0")"/.. && pwd)"',
+  );
+}

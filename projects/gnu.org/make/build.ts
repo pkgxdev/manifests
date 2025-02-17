9c96e73d99bc1f64d454b0d84ac12692a220461a
Max Howell <mxcl@me.com>
2025-02-17 14:54:00 -0500

    +make

---
+++ projects/gnu.org/make/build.ts
@@ -0,0 +1,13 @@
+import { BuildOptions, Path, run, unarchive } from "brewkit";
+
+export default async function ({ prefix, tag }: BuildOptions) {
+  await unarchive(`https://ftp.gnu.org/gnu/make/make-${tag}.tar.gz`);
+
+  if (Deno.build.os != "windows") {
+    run`./configure --prefix=${prefix}`;
+    run`make --jobs ${navigator.hardwareConcurrency} install`;
+  } else {
+    run`.\build_w32.bat`;
+    Path.cwd().join("WinRel/gnumake.exe").mv({ to: prefix.bin.mkdir("p").join("make.exe") });
+  }
+}

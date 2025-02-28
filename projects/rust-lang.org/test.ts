f0918d5ff5bb96835c9baa7d1a2ab82d2980c88e
Max Howell <mxcl@me.com>
2025-02-28 12:06:38 -0700

    +rust

---
+++ projects/rust-lang.org/test.ts
@@ -0,0 +1,12 @@
+import { run, undent } from "brewkit";
+
+export default function () {
+  Deno.writeTextFileSync(
+    "test.rs",
+    undent`
+    fn main() {
+      println!("Hello World!");
+    }`,
+  );
+  run`rustc test.rs -o hello --crate-name hello`;
+}

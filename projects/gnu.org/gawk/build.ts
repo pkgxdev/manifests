d11aac3cb21ebd2eec3ec250968da47522a30b47
Max Howell <mxcl@me.com>
2025-03-18 14:33:41 -0400

    +gawk

---
+++ projects/gnu.org/gawk/build.ts
@@ -0,0 +1,9 @@
+import { BuildOptions, run, unarchive } from "brewkit";
+
+export default async function ({ prefix, tag }: BuildOptions) {
+  await unarchive(`https://ftp.gnu.org/gnu/gawk/gawk-${tag}.tar.gz`);
+
+  run`./configure --prefix=${prefix}`;
+  run`make --jobs ${navigator.hardwareConcurrency} install`;
+  run`make test`;
+}
---
+++ projects/gnu.org/gawk/test.ts
@@ -0,0 +1,3 @@
+export default function () {
+  // test "$(echo "Goodbye, cruel World" | gawk '{ gsub("Goodbye, cruel", "Hello,"); print }')" = "Hello, World"
+}

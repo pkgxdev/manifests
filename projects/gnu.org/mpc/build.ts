97ed90bcdf9d26c9007d82480daee9417b855311
Max Howell <mxcl@me.com>
2025-03-18 13:30:33 -0400

    +mpc

---
+++ projects/gnu.org/mpc/build.ts
@@ -0,0 +1,10 @@
+import { BuildOptions, env_include, run, unarchive } from "brewkit";
+
+export default async function ({ prefix, tag }: BuildOptions) {
+  await unarchive(`https://ftp.gnu.org/gnu/mpc/mpc-${tag}.tar.gz`);
+
+  env_include("gnu.org/gmp"); // statically linked on linux
+
+  run`./configure --prefix=${prefix}`;
+  run`make --jobs ${navigator.hardwareConcurrency} install`;
+}

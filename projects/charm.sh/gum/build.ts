5097ac533cc10ebc44d02768dd899843da812ed2
Max Howell <mxcl@me.com>
2025-03-21 14:40:13 -0400

    +gum

---
+++ projects/charm.sh/gum/build.ts
@@ -0,0 +1,25 @@
+import { BuildOptions } from "brewkit";
+import unarchive from "../../../brewkit/unarchive.ts";
+
+export default async function({ prefix, version }: BuildOptions) {
+
+  const arch = (() => {
+    switch (Deno.build.target) {
+      case "x86_64-unknown-linux-gnu":
+        return "Linux_x86_64";
+      case "aarch64-unknown-linux-gnu":
+        return "Linux_arm64";
+      case "x86_64-apple-darwin":
+        return "Darwin_x86_64";
+      case "aarch64-apple-darwin":
+        return "Darwin_arm64";
+      case "x86_64-pc-windows-msvc":
+        return "Windows_x86_64";
+      default:
+        throw new Error(`Unsupported platform: ${Deno.build.target}`);
+    }
+  })();
+
+  await unarchive(`https://github.com/charmbracelet/gum/releases/download/v${version}/gum_${version}_${arch}.tar.gz`)
+  prefix.bin.install("gum");
+}
---
+++ projects/charm.sh/gum/fixup.ts
@@ -0,0 +1,4 @@
+export default function() {
+  // strip fails with weird error
+  return false;
+}

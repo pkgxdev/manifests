cc702a667aac73f52918ad1d3ff6b9174488b3f6
Max Howell <mxcl@me.com>
2025-03-27 17:04:34 -0400

    F libc++

--- projects/gnu.org/gcc/libstdcxx/versions.ts
+++ projects/gnu.org/gcc/libstdcxx/versions.ts
@@ -1,1 +1,13 @@
-../versions.ts
+import { github, semver } from "brewkit";
+
+export default async function () {
+  return (await github.tags("gcc-mirror/gcc")).compact(({ name: tag }) => {
+    const version = semver.parse(tag.replace(/^releases\/gcc-/, ""));
+    if (version) {
+      return {
+        tag,
+        version,
+      };
+    }
+  });
+}

8299f879b4637721b3ea77b36b117f27cc213003
Max Howell <mxcl@me.com>
2025-04-01 14:47:03 -0400

    Fix node

--- projects/nodejs.org/build.ts
+++ projects/nodejs.org/build.ts
@@ -7,12 +7,6 @@ export default async function ({ prefix, tag }: BuildOptions) {
     return run`.\\vcbuild`;
   }
 
-  if (Deno.build.os == "linux") {
-    // node^22 needs a newer libstdc++ than we provide
-    // gcc provides it as a static library
-    env_include("gnu.org/gcc");
-  }
-
   run`./configure --prefix=${prefix} --shared-openssl --shared-zlib --ninja --enable-lto`;
   run`make install`;  // runs ninja (much faster) via make
 }

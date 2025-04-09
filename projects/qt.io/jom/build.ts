5b5b03ad567cbeecdd8c8cc957a2e672e7f41fd6
Max Howell <mxcl@me.com>
2025-04-09 10:52:08 -0400

    wip

--- projects/qt.io/jom/build.ts
+++ projects/qt.io/jom/build.ts
@@ -2,6 +2,6 @@ import { unarchive, BuildOptions } from "brewkit";
 
 export default async function({ prefix, version }: BuildOptions) {
   const tag = version.toString().replace(".", "_");
-  await unarchive(`https://mirrors.tuna.tsinghua.edu.cn/qt/official_releases/jom/jom_${tag}.zip`);
+  await unarchive(`https://download.qt.io/official_releases/jom/jom_${tag}.zip`);
   prefix.bin.install("jom.exe");
 }

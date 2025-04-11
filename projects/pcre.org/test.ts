d5d2ef33c840cd6862b3221378c5c9ac748b7c0d
Max Howell <mxcl@me.com>
2025-04-10 20:37:38 -0400

    wip

--- projects/pcre.org/test.ts
+++ projects/pcre.org/test.ts
@@ -2,5 +2,5 @@ import { run, TestOptions } from "brewkit";
 
 export default async function ({ prefix, version }: TestOptions) {
   const v = version.major >= 10 ? "2" : "";
-  run`pcre${v}grep "regular expression" ${prefix}/share/man/man1/pcre2grep.1`;
+  run`pcre${v}grep "regular expression" ${prefix}/share/man/man1/pcre${v}grep.1`;
 }

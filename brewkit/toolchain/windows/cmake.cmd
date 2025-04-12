0f02bdef9a220837899b2841ad3230855477ba09
Max Howell <mxcl@me.com>
2025-04-12 12:15:17 -0400

    wip

--- brewkit/toolchain/windows/cmake.cmd
+++ brewkit/toolchain/windows/cmake.cmd
@@ -2,6 +2,7 @@
 setlocal
 
 set CMAKE_PREFIX_PATH=%PKGS%
+set CMAKE_GENERATOR=Ninja
 
 pkgx -q cmake %*
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
--- projects/libevent.org/build.ts
+++ projects/libevent.org/build.ts
@@ -3,6 +3,12 @@ import { BuildOptions, unarchive, run, Path } from "brewkit";
 export default async function ({ prefix, version, tag }: BuildOptions) {
   const url = `https://github.com/libevent/libevent/releases/download/${tag}/libevent-${version}-stable.tar.gz`;
   await unarchive(url);
-  run`cmake -B ./build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${prefix}`;
+  run`cmake
+      -B ./build
+      -GNinja
+      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_INSTALL_PREFIX=${prefix}
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5  # https://github.com/libevent/libevent/issues/1782
+      `;
   run`cmake --build ./build --config Release --target install`;
 }

d6ee4b955c64ab04768b1df70ab29f87a760a4c9
Max Howell <mxcl@me.com>
2025-04-14 16:22:16 -0400

    wip

--- projects/abseil.io/test.ts
+++ projects/abseil.io/test.ts
@@ -5,5 +5,5 @@ export default async function () {
   const flags = await backticks`pkg-config --cflags --libs absl_strings`;
   run`c++ -std=c++17 test.cc ${flags}`;
   const out = await backticks`./a.out`;
-  assertEquals(out, "Joined string: foo-bar-baz\n");
+  assertEquals(out, "Joined string: foo-bar-baz\\n");
 }
--- projects/protobuf.dev/build.ts
+++ projects/protobuf.dev/build.ts
@@ -17,13 +17,4 @@ export default async function ({ prefix, tag }: BuildOptions) {
 // env:
 //   linux:
 //     LDFLAGS: '$LDFLAGS -Wl,--allow-shlib-undefined'
-//   ARGS:
-//     - '-Dprotobuf_BUILD_LIBPROTOC=ON'
-//     - '-Dprotobuf_BUILD_SHARED_LIBS=ON'
-//     - '-Dprotobuf_INSTALL_EXAMPLES=OFF'
-//     - '-Dprotobuf_BUILD_TESTS=OFF'
-//     - '-DCMAKE_INSTALL_PREFIX={{prefix}}'
-//     - '-DCMAKE_BUILD_TYPE=Release'
-//     - '-Dprotobuf_ABSL_PROVIDER=package'
-//     - '-DCMAKE_PREFIX_PATH={{deps.abseil.io.prefix}}'
 }

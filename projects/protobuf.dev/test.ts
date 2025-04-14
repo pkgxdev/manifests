51557364f85ff53dd8d290636882a309edada30e
Max Howell <mxcl@me.com>
2025-04-14 16:08:06 -0400

    wip

--- projects/protobuf.dev/test.ts
+++ projects/protobuf.dev/test.ts
@@ -1,18 +1,5 @@
 import { run } from "brewkit";
 
 export default async function () {
-  // - run: cp $FIXTURE test.proto
-//   fixture:
-//     extname: proto
-//     content: |
-//       syntax = "proto3";
-//       package test;
-//       message TestCase {
-//         string name = 4;
-//       }
-//       message Test {
-//         repeated TestCase case = 1;
-//       }
-// - protoc test.proto --cpp_out=.
-// 
+  run`protoc test.proto --cpp_out=.`;
 }

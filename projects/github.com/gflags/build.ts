022763ac793c47af25703ea54569010be6ef23df
Max Howell <mxcl@me.com>
2025-04-14 10:27:34 -0400

    wip

--- projects/github.com/gflags/build.ts
+++ projects/github.com/gflags/build.ts
@@ -2,6 +2,25 @@ import { BuildOptions, unarchive, run } from "brewkit";
 
 export default async function ({ prefix, tag }: BuildOptions) {
   await unarchive(`https://github.com/gflags/gflags/archive/refs/tags/${tag}.tar.gz`);
-  run`cmake -B bld -DCMAKE_INSTALL_PREFIX=${prefix} -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON`;
+  run`cmake
+        -B bld
+        -DCMAKE_INSTALL_PREFIX=${prefix}
+        -DBUILD_SHARED_LIBS=ON
+        -DBUILD_STATIC_LIBS=ON
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5  # †
+        `;
   run`cmake --build bld --target install --config Release`;
 }
+
+/* †
+CMake Error at CMakeLists.txt:73 (cmake_minimum_required):
+-- Configuring incomplete, errors occurred!
+  Compatibility with CMake < 3.5 has been removed from CMake.
+
+  Update the VERSION argument <min> value.  Or, use the <min>...<max> syntax
+  to tell CMake that the project requires at least <min> but has been updated
+  to work with policies introduced by <max> or earlier.
+
+  Or, add -DCMAKE_POLICY_VERSION_MINIMUM=3.5 to try configuring anyway.
+ */

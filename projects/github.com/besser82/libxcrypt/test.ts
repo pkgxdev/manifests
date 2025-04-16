a9056a23dd2e9427af208adb3af0c3790ffab6a7
Max Howell <mxcl@me.com>
2025-04-16 12:55:29 -0400

    wip

---
+++ projects/github.com/besser82/libxcrypt/test.ts
@@ -0,0 +1,8 @@
+import { run } from "brewkit";
+
+export default async function () {
+  // script: |
+//   cc -L{{prefix}}/lib -lcrypt fixture.c
+//   ./a.out
+// 
+}
--- projects/gnome.org/gobject-introspection/test.ts
+++ projects/gnome.org/gobject-introspection/test.ts
@@ -4,7 +4,7 @@ import { assert } from "jsr:@std/assert@1/assert";
 
 export default async function () {
   try {
-    env_include("git-scm.org gnome.org/glib");
+    env_include("git-scm.org gnome.org/glib gnu.org/make");
   } catch {
     console.error("there is a cyclic dep between glib and this pkg.");
     console.error("we’ll pass the test for now but rebuild glib and then run this again.");
@@ -13,7 +13,7 @@ export default async function () {
 
   run`git clone https://gist.github.com/7a0023656ccfe309337a.git tst`;
   Path.cwd().join("tst").cd();
-  run`pkgx git apply ../test_make.diff`;
+  run`git apply ../test_make.diff`;
   run`make`;
 
   assert(Path.cwd().join("Tut-0.1.typelib").exists());

12a9fd4dec6516e7d7480faa9c3e3f59e504845e
Max Howell <mxcl@me.com>
2025-04-16 11:38:19 -0400

    wip

---
+++ projects/github.com/westes/flex/build.ts
@@ -0,0 +1,14 @@
+import { BuildOptions, unarchive, run } from "brewkit";
+
+export default async function ({ prefix, version, deps, tag, props }: BuildOptions) {
+  await unarchive(`https://github.com/westes/flex/releases/download/${tag}/flex-${version}.tar.gz`);
+
+  if (Deno.build.os === "linux") {
+    Deno.env.set("CPPFLAGS", "-D_GNU_SOURCE");
+  } else if (Deno.build.os === "darwin") {
+    Deno.env.set("MACOSX_DEPLOYMENT_TARGET", "10.6");
+  }
+
+  run`./configure --prefix=${prefix} --with-pic --disable-bootstrap --enable-shared`;
+  run`make --jobs ${navigator.hardwareConcurrency} install`;
+}

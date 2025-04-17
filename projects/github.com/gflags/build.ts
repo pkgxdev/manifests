4179460eb8152849473f91cefd897d49e93252d9
Max Howell <mxcl@me.com>
2025-04-17 09:12:59 -0400

    wip

--- projects/github.com/gflags/build.ts
+++ projects/github.com/gflags/build.ts
@@ -1,4 +1,4 @@
-import { BuildOptions, unarchive, run, Path } from "brewkit";
+import { BuildOptions, unarchive, run } from "brewkit";
 
 export default async function ({ prefix, tag }: BuildOptions) {
   await unarchive(`https://github.com/gflags/gflags/archive/refs/tags/${tag}.tar.gz`);
@@ -9,23 +9,7 @@ export default async function ({ prefix, tag }: BuildOptions) {
         -DBUILD_SHARED_LIBS=ON
         -DBUILD_STATIC_LIBS=ON
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5  # †
         `;
-
-  // Path.cwd().parent().join("pkgs/gnu.org/gcc/libstdcxx/v14/include").rm('rf');
-  // new Path("/root/.pkgx/gnu.org").rm('rf');
 
   run`cmake --build bld --target install --config Release`;
 }
-
-/* †
-CMake Error at CMakeLists.txt:73 (cmake_minimum_required):
--- Configuring incomplete, errors occurred!
-  Compatibility with CMake < 3.5 has been removed from CMake.
-
-  Update the VERSION argument <min> value.  Or, use the <min>...<max> syntax
-  to tell CMake that the project requires at least <min> but has been updated
-  to work with policies introduced by <max> or earlier.
-
-  Or, add -DCMAKE_POLICY_VERSION_MINIMUM=3.5 to try configuring anyway.
- */
--- projects/libevent.org/build.ts
+++ projects/libevent.org/build.ts
@@ -8,7 +8,6 @@ export default async function ({ prefix, version, tag }: BuildOptions) {
       -GNinja
       -DCMAKE_BUILD_TYPE=Release
       -DCMAKE_INSTALL_PREFIX=${prefix}
-      -DCMAKE_POLICY_VERSION_MINIMUM=3.5  # https://github.com/libevent/libevent/issues/1782
       `;
   run`cmake --build ./build --config Release --target install`;
 }

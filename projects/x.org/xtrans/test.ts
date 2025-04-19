229491004dbfb4a77c6a0b7392a73d46c5b8b545
Max Howell <mxcl@me.com>
2025-04-19 14:12:01 -0400

    wip

--- projects/x.org/xtrans/test.ts
+++ projects/x.org/xtrans/test.ts
@@ -1,6 +1,8 @@
-import { run } from "brewkit";
+import { env_include, run } from "brewkit";
 
 export default async function () {
+  env_include("x.org/protocol");
+
   run`cc test.c`
   run`./a.out`
 }

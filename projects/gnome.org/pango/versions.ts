25a28eee8219e2889203ba682c6dc579bacdf28b
Max Howell <mxcl@me.com>
2025-04-19 15:22:33 -0400

    wip

---
+++ projects/gnome.org/pango/versions.ts
@@ -0,0 +1,13 @@
+import { semver, gitlab } from "brewkit";
+
+export default async function () {
+  const rv = []
+  const g = gitlab({ server: 'gitlab.gnome.org', project: 'GNOME/pango', type: 'releases' });
+  for await (const { version: tag } of g) {
+    const version = semver.parse(tag);
+    if (version) {
+      rv.push({ version, tag });
+    }
+  }
+  return rv;
+}

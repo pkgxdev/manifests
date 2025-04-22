5d269a9f96013112aa347cfb1b1aa66ac85899a0
Max Howell <mxcl@me.com>
2025-04-22 15:57:41 -0400

    wip

--- projects/x.org/xscrnsaver/build.ts
+++ projects/x.org/xscrnsaver/build.ts
@@ -1,17 +1,12 @@
-import { BuildOptions, unarchive, run } from "brewkit";
+import { env_include, BuildOptions, unarchive, run } from "brewkit";
 
 export default async function ({ prefix, version, deps, tag, props }: BuildOptions) {
   await unarchive(`https://www.x.org/archive/individual/lib/libXScrnSaver-${version}.tar.gz`);
-  // script:
-//   - ./configure $ARGS
-//   - 'make --jobs ${navigator.hardwareConcurrency} install'
-//   - run: 'find . -name "*.la" -exec rm -f {} \;'
-//     working-directory: '${prefix}/lib'
-// env:
-//   SHELF: '${{pkgx.prefix}}/x.org'
-//   ARGS:
-//     - '--prefix="${prefix}"'
-//     - '--sysconfdir="$SHELF"/etc'
-//     - '--localstatedir="$SHELF"/var'
-//     - '--enable-spec=no'
+  env_include("x.org/protocol x.org/xcb^1.17");
+  run`./configure
+        --prefix=${prefix}
+        --sysconfdir=/etc'
+        --localstatedir=/var'
+        --enable-spec=no`;
+  run`make --jobs ${navigator.hardwareConcurrency} install`;
 }
---
+++ projects/x.org/xscrnsaver/test.c
@@ -0,0 +1,5 @@
+#include "X11/extensions/scrnsaver.h"
+int main(int argc, char* argv[]) {
+  XScreenSaverNotifyEvent event;
+  return 0;
+}

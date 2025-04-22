cefcee65e8e0d584c0c21a81f0b5020fcdb4f345
Max Howell <mxcl@me.com>
2025-04-22 19:49:36 -0400

    wip

--- projects/x.org/xrandr/test.ts
+++ projects/x.org/xrandr/test.ts
@@ -1,5 +1,5 @@
-import { backticks, TestOptions } from "brewkit";
 import { assertStringIncludes } from "jsr:@std/assert@1/string-includes";
+import { backticks, TestOptions } from "brewkit";
 
 export default async function ({version}: TestOptions) {
   const out = await backticks`pkg-config --modversion xrandr`;

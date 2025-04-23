bdf77d415265de15fe3500212a415ca87acb1ebe
Max Howell <mxcl@me.com>
2025-04-23 13:15:52 -0400

    wip

--- projects/alsa-project.org/build.ts
+++ projects/alsa-project.org/build.ts
@@ -1,6 +1,6 @@
 import { BuildOptions, unarchive, run } from "brewkit";
 
-export default async function ({ prefix, version, deps, tag, props }: BuildOptions) {
+export default async function ({ prefix, version }: BuildOptions) {
   await unarchive(`https://www.alsa-project.org/files/pub/lib/alsa-lib-${version}.tar.bz2`);
   run`./configure
         --prefix=${prefix}

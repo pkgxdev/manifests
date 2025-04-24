5fbc3f7e26ab19b6e59f7bb221c2ab709ae9b796
Max Howell <mxcl@me.com>
2025-04-24 07:12:06 -0400

    wip

--- projects/gnome.org/gdk-pixbuf/build.ts
+++ projects/gnome.org/gdk-pixbuf/build.ts
@@ -3,10 +3,7 @@ import { BuildOptions, unarchive, run, env_include } from "brewkit";
 export default async function ({ prefix, version }: BuildOptions) {
   await unarchive(`https://download.gnome.org/sources/gdk-pixbuf/${version.marketing}/gdk-pixbuf-${version}.tar.xz`);
   env_include("gnome.org/gobject-introspection");
-//   if test {{ hw.platform }} == linux; then
-//     #FIXME so linux build finds the shared-mime-info cache
-//     export XDG_DATA_DIRS="$XDG_DATA_DIRS:/usr/share"
-//   fi
+
   run`meson
       setup bld
       --buildtype=release

636f16dd72d87611a1201046b30fa9df17f72bb2
Max Howell <mxcl@me.com>
2025-04-23 21:39:20 -0400

    wip

--- projects/gnome.org/gdk-pixbuf/test.ts
+++ projects/gnome.org/gdk-pixbuf/test.ts
@@ -1,13 +1,6 @@
-import { run } from "brewkit";
+import { backticks, run } from "brewkit";
 
 export default async function () {
-  // dependencies:
-//   freedesktop.org/pkg-config: ^0.29
-// script: |
-//   cc test.c $CFLAGS $LDFLAGS
-//   ./a.out
-// env:
-//   CFLAGS: $(pkg-config --cflags gdk-pixbuf-2.0)
-//   LDFLAGS: $(pkg-config --libs gdk-pixbuf-2.0)
-// 
+  run`cc test.c -lgdk_pixbuf-2.0`;
+  run`./a.out`;
 }

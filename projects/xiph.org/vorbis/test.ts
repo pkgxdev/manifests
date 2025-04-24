ac79e00dd9466bf83ab8814e2f965051b90eefbe
Max Howell <mxcl@me.com>
2025-04-24 11:36:23 -0400

    wip

--- projects/xiph.org/vorbis/test.ts
+++ projects/xiph.org/vorbis/test.ts
@@ -5,7 +5,11 @@ export default async function () {
   run`cc test.c -lvorbisfile`;
 
   const proc = new Deno.Command("./a.out", { stdin: "piped" }).spawn();
-  await Deno.openSync(fixture("ogg").string).readable.pipeTo(proc.stdin);
+  try {
+    await Deno.openSync(fixture("ogg").string).readable.pipeTo(proc.stdin);
+  } catch (err) {
+    if (!(err instanceof Deno.errors.BrokenPipe)) throw err;
+  }
   const { success } = await proc.status;
   assert(success, "ogg test failed");
 }

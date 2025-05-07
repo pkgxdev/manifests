5d8339afec0b487c728252ee4a7a99cd3d4ce626
Max Howell <mxcl@me.com>
2025-05-07 13:30:06 -0400

    wip

--- projects/gnu.org/gcc/build.ts
+++ projects/gnu.org/gcc/build.ts
@@ -4,20 +4,7 @@ import { expandGlob } from "jsr:@std/fs@1/expand-glob";
 export default async function build({ prefix, version }: BuildOptions) {
   await unarchive(`https://ftpmirror.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz`);
 
-  let extra = "";
-
-  if (Deno.build.os == "linux") {
-    env_include("gnu.org/gcc^14");
-    extra = `
-      --disable-multilib
-      --build=x86_64-pc-linux-gnu  # or compile fails due to dupe pid_t
-      --with-arch=x86-64   # or c++ crashes with illegal instructions inside docker
-      --with-tune=generic  # ^^ same
-      #--enable-default-pie
-      #--enable-pie-tools
-      #--enable-host-pie
-      `;
-  }
+  env_include("gnu.org/gcc^14");
 
   Path.cwd().join("build").mkdir().cd();
 
@@ -25,17 +12,15 @@ export default async function build({ prefix, version }: BuildOptions) {
         --prefix=${prefix}
         --enable-languages=c,c++
         --disable-multilib
-        --enable-bootstrap
-        --with-bugurl=https://github.com/pkgxdev/manifests/issues
-        "--with-boot-ldflags=-static-libgcc -static-libstdc++"
+        --disable-bootstrap
         --disable-nls
         --enable-shared
-      # --with-system-zlib  #TODO
+  #TODO --with-system-zlib
+        --with-bugurl=https://github.com/pkgxdev/manifests/issues
         "--with-pkgversion=pkgx GCC ${version}"
-        ${extra}
         `;
   run`make --jobs ${navigator.hardwareConcurrency}`;
-  run`make install-strip`; // cannot install in parallel
+  run`make install-strip`;
 
   const lib64 = prefix.join("lib64");
   for await (const { path } of expandGlob(`${lib64}/*`)) {
--- projects/gnu.org/gcc/libstdcxx/build.ts
+++ projects/gnu.org/gcc/libstdcxx/build.ts
@@ -12,16 +12,13 @@ export default async function build({ prefix, version }: BuildOptions) {
         --prefix=${prefix}
         --enable-languages=c,c++
         --disable-multilib
-        --enable-bootstrap  # or it fails
+        --disable-bootstrap
         --disable-nls
-        --enable-default-pie
-        --enable-pie-tools
-        --enable-host-pie
         `;
   run`make
         --jobs ${navigator.hardwareConcurrency}
-        # all-target-libstdc++-v3
-        # all-target-libgcc`;
+        all-target-libstdc++-v3
+        all-target-libgcc`;
   run`make
         install-strip-target-libstdc++-v3
         install-strip-target-libgcc`;

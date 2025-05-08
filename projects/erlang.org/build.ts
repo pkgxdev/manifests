b1465ec031a995c7efef6bf6118e08bfd5d84024
Max Howell <mxcl@me.com>
2025-05-08 11:29:27 -0400

    wip

--- projects/erlang.org/build.ts
+++ projects/erlang.org/build.ts
@@ -3,18 +3,19 @@ import { env_include, BuildOptions, unarchive, run } from "brewkit";
 export default async function ({ prefix, version, deps, tag }: BuildOptions) {
   await unarchive(`https://github.com/erlang/otp/releases/download/${tag}/otp_src_${version}.tar.gz`);
 
-  env_include("gnu.org/gcc");
-
   let extra = Deno.build.os == 'darwin' ? `
     --enable-darwin-64bit
     --enable-kernel-poll
     --with-dynamic-trace=dtrace` : '';
 
   if (Deno.build.os == "linux") {
+    env_include("gnu.org/gcc");
+
     // ld.lld: error: undefined reference: __extendhfsf2
     extra = "LDFLAGS=-Wl,--allow-shlib-undefined";
   }
 
+  // or configure fails
   Deno.env.set("CFLAGS", "-O3");
 
   run`./configure

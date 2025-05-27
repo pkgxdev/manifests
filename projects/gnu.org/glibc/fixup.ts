f1fcb85408996a53438abe9a91b980b0a57be927
Max Howell <mxcl@me.com>
2025-05-27 10:01:42 -0400

    wip

--- projects/gnu.org/glibc/fixup.ts
+++ projects/gnu.org/glibc/fixup.ts
@@ -1,4 +1,8 @@
-export default async function () {
-  // basically everything fails for some reason
+export default function () {
+  // tools like `ldd` fail since they are installed to the system
+  // and require the system glibc but we set LD_LIBRARY_PATH so that the
+  // output from `ldd` is correct.
+  // can be fixed if we use our own completely controlled docker image
+  // all the same, no fixups are probably required since this _is_ *libc*
   return false;
 }
